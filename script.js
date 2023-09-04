72345ec88bbc99e4e24f36eebe53370560ef2e30
malit mohamut <60866464+mawlid1431@users.noreply.github.com>
2023-09-04 16:33:36 +0300

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 // Good Luck 💪🏾
+
+function ZeroAndOne(arr) {
+  let result = "";
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === "Zero") {
+      result += "0";
+    } else if (arr[i] === "One") {
+      result += "1";
+    }
+  }
+
+  return result;
+}
+
+// Example usages:
+console.log(ZeroAndOne(["one", "Zero", "Zero", "One"])); // Output: "001"
+console.log(ZeroAndOne(["Zero", "ONE", "one", "Zero"])); // Output: "00"
+
